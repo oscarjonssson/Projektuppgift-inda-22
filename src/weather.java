145982921eb2d924b8d865d035cfa2be44778b16
Filip Dimitrijevic <filipdi@kth.se>
2023-04-20 14:12:16 +0200

    java file, API key added

--- src/weather.java
+++ src/weather.java
@@ -1,36 +1,52 @@
-// import java.net.*;
 // import java.io.*;
-// import org.json.simple.*;
-// import org.json.simple.parser.*;
+// import java.net.*;
+// import org.json.*;
 
-// public class Weather {
-//     private static final String API_KEY = "your_openweathermap_api_key_here";
+// public class weather {
+
+//     private static final String OPEN_WEATHER_MAP_API =
+//             "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
+
+//     private static final String OPEN_WEATHER_MAP_API_KEY = "9226ed0b9dd0791ba0039533eae0c888";
+//     ;
 
 //     public static void main(String[] args) throws Exception {
-//         String location = "New York";
-//         String currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + API_KEY;
-//         String forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid="
-//                 + API_KEY;
 
-//         JSONObject currentWeather = getWeatherData(currentWeatherUrl);
-//         JSONObject forecastWeather = getWeatherData(forecastWeatherUrl);
+//         String city = "Stockholm";
+//         if (args.length > 0) {
+//             city = args[0];
+//         }
 
-//         // Code for displaying weather information on the website can be added here
-//     }
+//         String urlString = String.format(OPEN_WEATHER_MAP_API, city, OPEN_WEATHER_MAP_API_KEY);
+//         URL url = new URL(urlString);
+//         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-//     public static JSONObject getWeatherData(String url) throws Exception {
-//         URL weatherUrl = new URL(url);
-//         HttpURLConnection conn = (HttpURLConnection) weatherUrl.openConnection();
-//         conn.setRequestMethod("GET");
-//         BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//         StringBuilder weatherData = new StringBuilder();
+//         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
 //         String line;
+//         StringBuilder responseBuilder = new StringBuilder();
 //         while ((line = reader.readLine()) != null) {
-//             weatherData.append(line);
+//             responseBuilder.append(line);
 //         }
 //         reader.close();
-//         JSONParser parser = new JSONParser();
-//         JSONObject weatherJSON = (JSONObject) parser.parse(weatherData.toString());
-//         return weatherJSON;
+
+//         JSONObject response = new JSONObject(responseBuilder.toString());
+
+//         JSONObject main = response.getJSONObject("main");
+//         double temperature = main.getDouble("temp");
+//         int humidity = main.getInt("humidity");
+
+//         JSONObject wind = response.getJSONObject("wind");
+//         double windSpeed = wind.getDouble("speed");
+
+//         JSONArray weather = response.getJSONArray("weather");
+//         String description = weather.getJSONObject(0).getString("description");
+
+//         System.out.println("Current weather in " + city);
+//         System.out.println("Temperature: " + temperature + " °C");
+//         System.out.println("Humidity: " + humidity + " %");
+//         System.out.println("Wind speed: " + windSpeed + " m/s");
+//         System.out.println("Description: " + description);
 //     }
 // }
+
