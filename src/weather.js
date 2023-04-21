2b97d5dc6010fd31718fbcfdbe53dbf003eda2c2
Oscar Jonsson <oscjonss@kth.se>
2023-04-21 13:59:22 +0200

    Integrated the OpenWeather API to retrieve weather data for a specific location in javascript

--- src/weather.js
+++ src/weather.js
@@ -1,19 +1,36 @@
-let city = document.querySelector("#city");
+// weather.js
 
+const apiKey = `9226ed0b9dd0791ba0039533eae0c888`;
 
-check.addEventListener("click", () => {
-    let apiKey = `9226ed0b9dd0791ba0039533eae0c888`;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&units=metric&appid=${apiKey}`;
+const searchForm = document.querySelector('form');
+const locationInput = document.querySelector('input[name="location"]');
+const todayWidget = document.querySelector('#today-widget');
+const forecastWidget = document.querySelector('#forecast-widget');
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            weatherCity.innerText = `${data.name}, ${data.sys.country}`;
-            temperature.innerHTML = `${data.main.temp}°C`;
-        })
-        .catch(error => console.log(error));
-
-    city.value = "";
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = locationInput.value;
+    getLocationData(location);
 });
 
+async function getLocationData(location) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`);
+        const data = await response.json();
+        console.log(data); // Print the data object to the console to see the structure of the response
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+        displayWeather(location, temperature, description);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayWeather(location, temperature, description) {
+    todayWidget.innerHTML = `
+    <h2>${location}</h2>
+    <p>Temperature: ${temperature} &deg;C</p>
+    <p>Description: ${description}</p>
+  `;
+    locationInput.value = ''; // Clear the input field
+}
